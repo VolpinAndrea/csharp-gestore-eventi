2b280f71a0c2eba24898bd7301fbc233271b0213
VolpinAndrea <andvolpin19@gmail.com>
2022-12-13 10:58:36 +0100

    Mileston 1 senza metodi

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        private string titolo;
+        private DateTime data;
+        private int capienzaMax;
+        private int prenotati;
+
+        // GET
+
+        public string GetTitolo()
+        {
+            return titolo;
+        }
+        public DateTime GetData()
+        {
+            return data;
+        }
+        public int GetCapienzaMax()
+        {
+            return capienzaMax;
+        }
+        public int GetPrenotati()
+        {
+            return prenotati;
+        }
+
+        // SET
+
+        public void SetTitolo(string titolo)
+        {
+            this.titolo = titolo;
+        }
+
+        public void SetData(string data)
+        {
+            DateTime dataOraAttuale = DateTime.Now;
+            DateTime dataFormattata = DateTime.Parse(data);
+
+            if (dataFormattata >= dataOraAttuale)
+            {
+                this.data = dataFormattata;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Data la data inserita è già passata");
+            }
+        }
+
+        public Evento(string titolo, string data, int capienzaMax)
+        {
+            SetTitolo(titolo);
+            SetData(data);
+            this.capienzaMax = capienzaMax;
+            this.prenotati = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Titolo: "+ this.titolo + "\tData: " + this.data + "\nPosti totali: "+ capienzaMax + "\tPosti prenotati: "+ prenotati;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using GestoreEventi;
+try
+{
+    Evento nuovo = new Evento("Danza", "3/12/2022", 30);
+    Console.WriteLine(nuovo.ToString());
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine("La data inserita è già passata");
+}
